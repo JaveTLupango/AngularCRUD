20556ca629d8754c9067b0da77f11e3098854294
Jave <pangga.jave@gmail.com>
2021-11-20 12:47:56 +0800

    SweetAlert2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentComponent } from './student/student.component';
+import { SweetAlertComponent } from './sweet-alert/sweet-alert.component';
 
 const routes: Routes = [
-  {path: 'student', component:StudentComponent}
+  {path: 'student', component:StudentComponent},
+  {path: 'swal', component: SweetAlertComponent}
   ///
 ];
 
---
+++ src/app/sweet-alert/sweet-alert.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-sweet-alert',
+  templateUrl: './sweet-alert.component.html',
+  styleUrls: ['./sweet-alert.component.css']
+})
+export class SweetAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    Swal.fire(
+      'Good job!',
+      'You clicked the button!',
+      'success'
+    )
+
+    }
+
+}
